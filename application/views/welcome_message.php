5374c1efdc715569ccf4b7cb76e8e20f614a0b33
asmajati15 <asmajati.cooler15@gmail.com>
2022-08-03 10:26:52 +0700

    pertama

---
+++ application/views/welcome_message.php
@@ -0,0 +1,77 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Welcome to CodeIgniter</title>
+
+	<style type="text/css">
+
+	::selection { background-color: #E13300; color: white; }
+	::-moz-selection { background-color: #E13300; color: white; }
+
+	#container {
+		margin: 10px;
+		border: 1px solid #D0D0D0;
+		box-shadow: 0 0 8px #D0D0D0;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+
+	table {
+		width: 100%;
+	}
+	
+	table, tr, td {
+		border: 1px solid;
+		border-collapse: collapse;
+	}
+	
+	.satu {
+		height: 100px;
+	}
+
+	.dua {
+		height: 45px;
+	}
+
+	.tiga {
+		height: 500px;
+	}
+
+	.empat {
+		height: 70px;
+	}
+	</style>
+</head>
+<body>
+
+<div id="container">
+<table>
+	<tr class="satu">
+		<td>
+			<center>Welcome Brother</center>
+		</td>
+	</tr>
+	<tr class="dua">
+		<td>
+			<center>Beranda Profile</center>
+		</td>
+	</tr>
+	<tr class="tiga">
+		<td>
+			<center>Table</center>
+		</td>
+	</tr>
+	<tr class="empat">
+		<td>
+			<center>Support by Codein</center>
+		</td>
+	</tr>
+</table>
+</div>
+
+</body>
+</html>
